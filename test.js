36723730d3b38cb30a6759ab45924d5bc6d82717
Trimple <andrevdovnar@gmail.com>
2022-03-05 01:36:14 +0300

    Added date calculation

--- test.js
+++ test.js
@@ -1,1 +1,6 @@
 console.log("Hello World");
+
+let targetDate = new Date("03/30/2022");
+let currentDate = new Date("02/24/2022");
+
+console.log((targetDate.getTime() - currentDate.getTime())/ (1000 * 3600 * 24));
