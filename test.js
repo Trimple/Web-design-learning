939a67345933a7ec2fd365847dbf26bcf722071a
Trimple <andrevdovnar@gmail.com>
2022-03-11 13:47:41 +0300

    Made a first target update

--- test.js
+++ test.js
@@ -1,6 +1,17 @@
-console.log("Hello World");
 
-let targetDate = new Date("03/30/2022");
-let currentDate = new Date("02/24/2022");
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+var request = new XMLHttpRequest();
 
-console.log((targetDate.getTime() - currentDate.getTime())/ (1000 * 3600 * 24));
+request.open('GET', 'https://api.clickup.com/api/v2/list/168468387/task?');
+
+request.setRequestHeader('Authorization', 'pk_2626364_EZBVP3KRT6S8I46ML3RNYUS2RIUBMGW6');
+
+request.onreadystatechange = function () {
+  if (this.readyState === 4) {
+    console.log('Status:', this.status);
+    console.log('Headers:', this.getAllResponseHeaders());
+    console.log('Body:', this.responseText);
+  }
+};
+
+request.send();
